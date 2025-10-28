4843bae9eecbaae6bb83ac19eaaf120cdc350858
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2025-10-28 19:18:32 +0330

    fix(serializer): resolve 'obj has no attribute artist' error in Post serializers
    - Fixed `get_artist` method in both Quick and Normal Post serializers.
    - Corrected logic to safely access `media.artist` only when media exists.
    - Prevented attribute errors by adding conditional fallback.
    - Added default return for posts without an associated artist:
      `{ 'id': 0, 'name': 'ناشناس' }`

--- backend/media_center/serializers.py
+++ backend/media_center/serializers.py
@@ -91,11 +91,14 @@ class QuickPostSerializer(ModelSerializer):
         return f'{duration//60}:{duration%60:02d}'
     
     def get_artist(self, obj: Post):
-        artist = obj.medias.first().artist
+        media = obj.medias.first()
         return {
-            'id': artist.id,
-            'name': artist.name
-        } if artist else None
+            'id': media.artist.id,
+            'name': media.artist.name
+        } if media else {
+            'id': 0,
+            'name': 'ناشناس'
+        }
     
     def get_is_liked(self, obj: Post):
         request = self.context.get('request', None)
@@ -130,6 +133,9 @@ class PostSerializer(ModelSerializer):
         return {
             'id': artist.id,
             'name': artist.name
+        } if artist else {
+            'id': 0,
+            'name': 'ناشناس'
         }
     
     def get_is_liked(self, obj: Post):
